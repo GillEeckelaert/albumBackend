3a3104772549633f27719ccf7ac3dea6d281c89b
geeckela <Gill.Eeckelaert@UGent.be>
2021-06-02 00:19:49 +0200

    error fix

--- application/schemas/Schemas.py
+++ application/schemas/Schemas.py
@@ -70,7 +70,7 @@ class Query(graphene.ObjectType):
     collection = relay.Node.Field(CollectionObject)
     user_book = relay.Node.Field(UserBookObject)
 
-    def resolve_find_user(self, args, context, info):
+    def resolve_user(self, args, context, info):
         query = UserObject.get_query(context)
         username = args.get('username')
         return query.filter(UserModel.username == username).first()
